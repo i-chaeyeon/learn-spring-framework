059e3d0720b0709e9cef486e8ac3016ca7205f93
Lee Chaeyeon <leechae0605@naver.com>
2024-08-27 17:54:23 +0900

    sec3. constructor injections

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a0/SimpleSpringContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -19,16 +19,24 @@ class YourBusinessClass{
     Dependency2 dependency2;
 
     @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("Setter injection - setDependency1");
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println("Constructor injection - YourBusinessClass");
         this.dependency1 = dependency1;
-    }
-
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        System.out.println("Setter injection - setDependency2");
         this.dependency2 = dependency2;
     }
+
+    //    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter injection - setDependency1");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter injection - setDependency2");
+//        this.dependency2 = dependency2;
+//    }
 
     public String toString(){
         return "Using" + dependency1 + "and" + dependency2;
