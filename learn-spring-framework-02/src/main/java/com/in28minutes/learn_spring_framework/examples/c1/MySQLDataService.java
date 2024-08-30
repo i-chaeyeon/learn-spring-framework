b04f46a248fb090c2c828b0e7d46248e71b795e1
Lee Chaeyeon <leechae0605@naver.com>
2024-08-30 12:56:18 +0900

    sec4. Specialization of Component annotation

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/MySQLDataService.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/MySQLDataService.java
@@ -3,8 +3,10 @@ package com.in28minutes.learn_spring_framework.examples.c1;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+//@Component
+@Repository
 @Primary
 public class MySQLDataService implements DataService {
     @Override
